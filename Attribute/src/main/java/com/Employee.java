e5fad10684b7f6210b45b4bfc5b850f9717e4fdc
santhoshE429 <santhoshelumalaim@gmail.com>
2025-02-12 11:40:12 +0530

    Attribute

---
+++ Attribute/src/main/java/com/Employee.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Employee {
+	
+	 int empId;
+	 String name;
+	public Employee(int empId, String name) {
+		super();
+		this.empId = empId;
+		this.name = name;
+	}
+	 
+	
+
+}
