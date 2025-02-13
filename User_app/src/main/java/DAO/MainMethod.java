55fd29f69af8018165edeb8ec4d698a79a16990e
santhoshE429 <santhoshelumalaim@gmail.com>
2025-02-13 17:11:36 +0530

    MainMethod

---
+++ User_app/src/main/java/DAO/MainMethod.java
@@ -0,0 +1,27 @@
+package DAO;
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+		
+		User u=new User();
+		u.setId(1);
+		u.setFirstName("sant");
+		u.setLastName("E");
+		u.setGender("male");
+		u.setEmail("sant@gmail.com");
+		u.setPassword("123");
+		u.setPhoneNo(19333);
+		
+		UserDao user=new UserDao();
+		try {
+//			user.saveUser(u);
+			User u1=user.getById(1);
+			System.out.println(u1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+}
