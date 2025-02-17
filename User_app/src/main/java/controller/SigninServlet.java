5626538e71cdf17b1eaab4626a3d941eabac75d3
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-02-17 17:50:12 +0530

    signiServlet

--- User_app/src/main/java/controller/SigninServlet.java
+++ User_app/src/main/java/controller/SigninServlet.java
@@ -1,5 +1,44 @@
 package controller;
 
-public class SigninServlet {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import DAO.User;
+import DAO.UserDao;
+@WebServlet("/signin")
+public class SigninServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		int id=Integer.parseInt(req.getParameter("id")) ;
+		String fname=req.getParameter("fname");
+		String lname=req.getParameter("lname");
+		String gender=req.getParameter("gender");
+		String email=req.getParameter("email");
+		String password=req.getParameter("password");
+		long phoneNo=Long.parseLong(req.getParameter("phoneNo"));
+		
+		User u=new User();
+		u.setId(id);
+		u.setFirstName(fname);
+		u.setLastName(lname);
+		u.setGender(gender);
+		u.setEmail(email);
+		u.setPassword(password);
+		u.setPhoneNo(phoneNo);
+		UserDao dao=new UserDao();
+		try {
+			dao.saveUser(u);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 
 }
