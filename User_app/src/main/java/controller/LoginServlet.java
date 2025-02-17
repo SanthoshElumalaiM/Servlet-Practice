3f29df3b2c1547b2b43414266b0d8708c6baf214
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-02-17 19:07:17 +0530

    LoginServlet

---
+++ User_app/src/main/java/controller/LoginServlet.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import DAO.User;
+import DAO.UserDao;
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String email=req.getParameter("email");
+		String password=req.getParameter("password");
+		
+		UserDao dao=new UserDao();
+		PrintWriter out=resp.getWriter();
+		try {
+			
+			User u=dao.getByEmail(email);
+			if(u!=null)
+			{
+				if(password.equals(u.getPassword()))
+				{
+					RequestDispatcher dispatcher=req.getRequestDispatcher("Calculator.html");
+					dispatcher.forward(req, resp);
+				}
+				else {
+					
+					out.print("<h1 style='color:red'>Invalid Password</h1>");
+					RequestDispatcher dispatcher=req.getRequestDispatcher("Login.html");
+					dispatcher.include(req, resp);
+				}
+			}
+			else 
+			{
+				out.print("<h1 style='color:red' >Invalid Email Id</h1>");
+				RequestDispatcher dispatcher=req.getRequestDispatcher("Login.html");
+				dispatcher.include(req, resp);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
