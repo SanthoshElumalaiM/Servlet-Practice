e4d9b47e33d2548a2f5423c7a188e010d89fe8ac
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2026-02-27 13:52:45 +0530

    httpServlet

---
+++ PracticeMode/src/main/java/httpServletDemo/DemoHttpServlet.java
@@ -0,0 +1,33 @@
+package httpServletDemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/httpDemo")
+public class DemoHttpServlet  extends HttpServlet{
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		super.doPost(req, resp);
+		
+		PrintWriter out=resp.getWriter();
+		out.print("<h1>Hi This is post method</h1>");
+		
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		super.doGet(req, resp);
+		
+		PrintWriter out=resp.getWriter();
+		out.print("<h1>Hi This is get method</h1>");
+	}
+}
